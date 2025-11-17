e25e0dab77dae429569e7da4a2db472f473c9083
MaitreGobz <nayetlucas@hotmail.fr>
2025-11-17 19:03:13 +0100

    prepare work environnement done

---
+++ docs/phpdoc/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\App\\DataFixtures\\AppFixtures",
+            "name": "AppFixtures",
+            "summary": "",
+            "url": "classes/App-DataFixtures-AppFixtures.html"
+        },                {
+            "fqsen": "\\App\\DataFixtures\\AppFixtures\u003A\u003Aload\u0028\u0029",
+            "name": "load",
+            "summary": "",
+            "url": "classes/App-DataFixtures-AppFixtures.html#method_load"
+        },                {
+            "fqsen": "\\App\\Kernel",
+            "name": "Kernel",
+            "summary": "",
+            "url": "classes/App-Kernel.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\App\\DataFixtures",
+            "name": "DataFixtures",
+            "summary": "",
+            "url": "namespaces/app-datafixtures.html"
+        },                {
+            "fqsen": "\\App",
+            "name": "App",
+            "summary": "",
+            "url": "namespaces/app.html"
+        }            ]
+);
