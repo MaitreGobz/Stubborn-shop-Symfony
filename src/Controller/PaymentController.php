14886bc3fae2fd33bf02c4c9e7b0c74c1a129b05
MaitreGobz <nayetlucas@hotmail.fr>
2025-11-26 18:34:21 +0100

    confirmation page done

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,11 +18,19 @@ final class PaymentController extends AbstractController
      * 
      * Route : /payment/success
      * 
+     * @param CartService $cartService Service de gestion du panier.
+     * 
      * @return Response La réponse HTTP contenant la vue de succès.
      */
     #[Route('/payment/success', name: 'payment_success', methods: ['GET'])]
-    public function success(): Response
+    public function success(CartService $cartService): Response
     {
+        // Empty the cart after a successful payment
+        $cartService->clear();
+
+        //Success Flash Message
+        $this->addFlash('success', 'Paiement effectué avec succès ! Merci pour votre commande.');
+
         return $this->render('payment/success.html.twig');
     }
 
@@ -35,6 +44,9 @@ final class PaymentController extends AbstractController
     #[Route('/payment/cancel', name: 'payment_cancel', methods: ['GET'])]
     public function cancel(): Response
     {
+        //Cancel Flash Message
+        $this->addFlash('error', 'Le paiement a été annulé ou a échoué. Veuillez réessayer.');
+        
         return $this->render('payment/cancel.html.twig');
     }
 }
